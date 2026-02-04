e7485436da09ce958f7e00318236684f85ac47c3
Jimmy Loloy <jimmyloloy@ticasolutions.com.mx>
2026-02-04 12:50:42 -0500

    chore: update server file

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
 const { createServer } = require("http");
 const next = require("next");
 const { parse } = require("url");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const dev = process.env.NODE_ENV !== "production";
+
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -12,8 +12,5 @@ app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+  }).listen(port);
 });
