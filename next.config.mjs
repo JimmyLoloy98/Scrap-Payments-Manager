b6c16effc47f72d41d4a86f21a444c20548cf1df
Jimmy Loloy <jimmyloloy@ticasolutions.com.mx>
2026-02-04 13:14:53 -0500

    chore: update config file for next

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+
 }
 
-module.exports = nextConfig
+export default nextConfig
